7c6075e79492eebbfa726c70ee47b6288b08c166
Ibrahim Rahhal <74500241+ibrahimra7al@users.noreply.github.com>
2022-05-11 11:55:14 +0300

    LIT-12312: asodkppoaskd

--- global.d.ts
+++ global.d.ts
@@ -2,7 +2,7 @@ declare module "*.pug" {
     const value: (data: any) => string;
     export default value;
 }
-asd
+
 declare module "*.jsonasda" {
     const value: any;
     export default value;asas
