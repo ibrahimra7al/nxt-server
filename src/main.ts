07fdea0811f2ed456860babfc85ec45d55b31c2c
Ibrahim Rahhal <74500241+ibrahimra7al@users.noreply.github.com>
2022-05-17 07:45:39 +0300

    asdas
    dasd

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { Factory } from '@core';
 import { PrimaryModule } from '@primary';
 (async () => {
   const app = await Factory.create(PrimaryModule);
-  await app.listen(3000);testas
+  await app.listen(3000);testassadas
 })();
